3b5588445db648c22707d8a9c54abe253cd62a61
Андрей <orphsn@imac-andrej.local>
2016-11-22 02:43:04 -0800

    Initial Commit

---
+++ feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  feed
+//
+//  Created by Андрей on 22.11.16.
+//  Copyright © 2016 Андрей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
